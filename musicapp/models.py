3233e0bc915f5ab3953ab40de3c4dad88af7695c
Akshay H <akshayhuliyaru2005@gmail.com>
2026-04-16 22:20:03 +0530

    Updated with number of songs and tested application

--- musicapp/models.py
+++ musicapp/models.py
@@ -3,10 +3,11 @@ from django.db import models
 class Song(models.Model):
     title=models.TextField()
     artist=models.TextField()
+    image = models.ImageField(upload_to='images/',null=True,blank=True)
     audio_file=models.FileField()
     audio_link=models.CharField(max_length=200,blank=True,null=True)
     lyrics=models.TextField(blank=True,null=True)
-    duration=models.CharField(max_length=200)
+    duration=models.CharField(max_length=20)
     paginated_by=2
     
     def __str__(self):
--- musicapp/views.py
+++ musicapp/views.py
@@ -2,14 +2,14 @@ from django.shortcuts import render
 from .models import Song
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 def index(request):
-    paginator=paginator(Song.objects.all(),1)
+    paginator=Paginator(Song.objects.all(),1)
     page_number=request.GET.get('page')
     try:
-        page_obj=paginator.get_obj(page_number)
+        page_obj=paginator.page(page_number)
     except PageNotAnInteger:
         page_obj=paginator.page(1)
     except EmptyPage:
         page_obj=paginator.page(paginator.num_pages) 
     context={'page_obj':page_obj}
-    return render(request,'index.html',context)  
+    return render(request,'musicapp/index.html',context)  
     
